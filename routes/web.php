7037454e940b13c031253ca57e09f5257ba9992a
omareldars <omar.a.eldars@gmail.com>
2021-03-20 08:02:31 +0200

    Middleware Authentication

--- routes/web.php
+++ routes/web.php
@@ -18,14 +18,14 @@ Route::get('/', function () {
 });
 
 
-Route::get('posts',[PostController::class,'index'])->name('posts.index');
-Route::get('/posts/create',[PostController::class,'create'])->name('posts.create');
-Route::post('/posts',[PostController::class,'store'])->name('posts.store');
+Route::get('posts',[PostController::class,'index'])->name('posts.index')->middleware('auth');
+Route::get('/posts/create',[PostController::class,'create'])->name('posts.create')->middleware('auth');
+Route::post('/posts',[PostController::class,'store'])->name('posts.store')->middleware('auth');
 //Route::get('posts')
-Route::get('/posts/{post_id}',[PostController::class,'show'])->name('posts.show');
-Route::delete('/posts/delete/{post}',[PostController::class,'destroy'])->name('posts.destroy');
-Route::get('/posts/edit/{post}',[PostController::class,'edit'])->name('posts.edit');
-Route::put('/posts/{post}',[PostController::class,'update'])->name('posts.update');
+Route::get('/posts/{post_id}',[PostController::class,'show'])->name('posts.show')->middleware('auth');
+Route::delete('/posts/delete/{post}',[PostController::class,'destroy'])->name('posts.destroy')->middleware('auth');
+Route::get('/posts/edit/{post}',[PostController::class,'edit'])->name('posts.edit')->middleware('auth');
+Route::put('/posts/{post}',[PostController::class,'update'])->name('posts.update')->middleware('auth');
 
 
 
